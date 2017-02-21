2476a663e28f3dc3992b38f056c4f12eec5f4b1b
janspliethoff <spliethoff-junior@gmx.de>
2017-02-21 16:36:28 +0100

    :sparkles: scroll Buttons now Change Color with Chapter & are over Chapter Illustration; Achieved got an infoBubble,

--- js/maincontroller.js
+++ js/maincontroller.js
@@ -5,7 +5,7 @@ var student = getStudent();
 var avatars = getAvatars();
 var chapters = getChapters();
 var chapterillustrations = getChapterillustrations();
-var avatarSelected ="";
+var avatarSelected = "";
 
 $(document).ready(function () {
   setTimeout(function () {
@@ -150,7 +150,7 @@ function changeOnChapter(chapterId, achieved) {
 function changeOnChapterDelayed(chapterId, achieved) {
 
   document.getElementById("todo_liste").innerHTML = "";
-
+  var chapterURL="";
   if (chapterId == 0) {
     document.getElementById("body1").style.backgroundColor = "#8da6d6";
 
@@ -170,11 +170,13 @@ function changeOnChapterDelayed(chapterId, achieved) {
         return (a.fromDate - b.fromDate);
       })
 
+      alert("Aufgerufen")
+
       for (var i = 0; i < response.length; i++) {
-        $("#todo_liste").append("<div class='bubble inline'>" +
-          "<div class='right'>" + "<img src=" + "images/achievedCompetences-inactive.png" + ">" + "</div>" +
-          "<div class='left'>" + "<p>" + response[i].studentText + "</p>" + "</div>" +
-          "</div>");
+
+        makeBubble("Erreicht am:", response[i].fromDate, "images/achievedCompetences-active.png", "images/achievedCompetences-inactive.png",
+          response[i].studentText, response[i].number,i);
+
       }
     })
   } else {
@@ -182,9 +184,16 @@ function changeOnChapterDelayed(chapterId, achieved) {
     document.getElementById("body1").style.backgroundColor = chapters[chapterId - 1].weakcolor;
     if (chapterId < 10) {
       document.getElementById("flagImg").src = "images/chapter0" + chapterId + "/littleChapterFlag.png";
+      chapterURL = "chapter0"+chapterId;
     } else {
-      document.getElementById("flagImg").src = "images/chapter" + chapterId + "/littleChapterFlag.png";
+      chapterURL = "chapter" +chapterId;
     }
+
+    document.getElementById("flagImg").src = "images/" + chapterURL + "/littleChapterFlag.png";
+    document.getElementById("scrollUp").src = "images/" + chapterURL + "/scrollUp.png";
+    document.getElementById("scrollDown").src = "images/" + chapterURL + "/scrollDown.png";
+
+
 
     var achivedYN = (achieved) ? "true" : "false";
 
@@ -203,14 +212,22 @@ function changeOnChapterDelayed(chapterId, achieved) {
         return (a.fromDate - b.fromDate);
       })
 
-      for (var i = 0; i < response.length; i++) {
-        $("#todo_liste").append("<div class='bubble'>" +"<div>"+
-          "<div class='right'>" + "<img src=" + "images/achievedCompetences-inactive.png" + ">" + "</div>" +
-          "<div class='left'>" + "<p>" + response[i].studentText + "</p>" +
-          "</div>" +
-          "</div>"+
-          "<p id='bubbleNumber'>"+ response[i].number +"</p>"+
-          "</div>");
+      for (var i = 0; i < response.length; i++) {/*
+       $("#todo_liste").append("<div class='bubble'>" + "<div>" +
+       "<div class='right'>" + "<img src=" + "images/achievedCompetences-inactive.png" + ">" + "</div>" +
+       "<div class='left'>" + "<p>" + response[i].studentText + "</p>" +
+       "</div>" +
+       "</div>" +
+       "<p id='bubbleNumber'>" + response[i].number + "</p>" +
+       "</div>");*/
+
+
+
+        makeBubble("Erreicht am:", response[i].fromDate,
+          (response[i].checked.valueOf().toLocaleString().localeCompare("true".toLocaleString())) ? "images/"+chapterURL+"/competenceUndone.png" :  "images/"+chapterURL+"/competenceDone.png",
+          (response[i].checked.valueOf().toLocaleString().localeCompare("true".toLocaleString())) ? "images/"+chapterURL+"/competenceUndone.png" :  "images/"+chapterURL+"/competenceDone.png",
+          response[i].studentText, response[i].number,i);
+
       }
     })
 
@@ -237,9 +254,6 @@ function getFoerderplan(planId) {
   }, 200);
 }
 function getFoerderplanDelayed(planId) {
-
-
-  console.log("planId: "+planId)
 
   var settings = {
     "async": true,
@@ -282,39 +296,8 @@ function getFoerderplanDelayed(planId) {
 
           var _id = foerderplan[0].competences[j].competenceId;
 
-          $("#todo_liste").append(
-            "<div class='bubble'>" +
-            "<div id='infoDiv' class='right'>" + "<div class='infoBubble'" + ">" +
-              "<div class='infoBubbleText'>" +
-            foerderplanTitel[planId].name +"<br>"+
-            foerderplan[0].competences[j].note +
-            "</div>"+
-            "</div>" + "</div>" +
-            "<div class='left'>" +
-            "<div class='right bubbleImgDiv'>" +
-            "<img class='bubbleImg'src=" + "images/educationalPlan-inactive.png" + ">" +
-            "</div>" +
-            "<div class='left'>" +
-            "<p>" + competences[_id].studentText + "</p>" + "" +
-            "<p>" + competences[_id].number + "</p>" +
-            "</div>" +
-            "</div>" +
-            "</div>");
-
-          $(".bubbleImgDiv").parent().parent().children('#infoDiv').hide();
-          $(".bubbleImg").attr("src","images/educationalPlan-inactive.png");
-
-          $(".bubbleImgDiv").hover(
-            function () {
-              $(this).parent().parent().children('#infoDiv').show();
-              $(this).children('.bubbleImg').attr("src","images/educationalPlan-active.png");
-
-            },
-            function () {
-              $(this).parent().parent().children('#infoDiv').hide();
-              $(this).children('.bubbleImg').attr("src","images/educationalPlan-inactive.png");
-            }
-          );
+          makeBubble(foerderplanTitel[planId].name, foerderplan[0].competences[j].note, "images/educationalPlan-active.png"
+            , "images/educationalPlan-inactive.png", competences[_id].studentText, competences[_id].number,_id);
         }
       });
     });
@@ -356,13 +339,13 @@ function foerderPlaneInit() {
 
 function dynamischeBilderDropdown() {
 
-   var sheet = document.createElement('style')
-   sheet.innerHTML =
-     ".open .studentButton {content:url(\""+avatars[(student.avatarId)].avatarUrl +"\")}" +
-     ".open .schoolButton {content:url(\"" +student.school.imageUrl+"\")}" +
-     ".open .classesButton {content:url(\"" +student.studyGroups.imageUrl +"\")}";
+  var sheet = document.createElement('style')
+  sheet.innerHTML =
+    ".open .studentButton {content:url(\"" + avatars[(student.avatarId)].avatarUrl + "\")}" +
+    ".open .schoolButton {content:url(\"" + student.school.imageUrl + "\")}" +
+    ".open .classesButton {content:url(\"" + student.studyGroups.imageUrl + "\")}";
 
-   document.body.appendChild(sheet);
+  document.body.appendChild(sheet);
 }
 
 function changeContent(content) {
@@ -381,7 +364,7 @@ function changeContent(content) {
     avatarSelected = "";
     $("#fenster").load("parts.html #avatarContent")
   }
-  else if (content == "pwChange"){
+  else if (content == "pwChange") {
     document.getElementById("body1").style.backgroundColor = "#FFF";
     $("#fenster").load("parts.html #passwordChangeContent")
 
@@ -399,7 +382,7 @@ function logout() {
   }
 }
 
-function deleteProfile(){
+function deleteProfile() {
 
 // PW kann nicht mitgeschicktwerden ist ja noch nutzlos
   // document.getElementById("#passwordinput").value;
@@ -415,7 +398,7 @@ function deleteProfile(){
   }
 
   $.ajax(settings).done(function (response) {
-    console.log("DELETET",response);
+    console.log("DELETET", response);
   });
 }
 
@@ -426,17 +409,17 @@ function selectAvatar(avatarID) {
   console.log(avi[avatarID]);
 }
 
-function changeAvatar(){
+function changeAvatar() {
 
 
-  if(avatarSelected == ""){
+  if (avatarSelected == "") {
     alert("None Selected");
   }
 
   var settings = {
     "async": true,
     "crossDomain": true,
-    "url": "http://46.101.204.215:1337/api/V1/avatar/:"+avatarSelected,
+    "url": "http://46.101.204.215:1337/api/V1/avatar/:" + avatarSelected,
     "method": "PUT",
     "headers": {
       "authorization": token.token,
@@ -444,10 +427,10 @@ function changeAvatar(){
   }
 
   $.ajax(settings).done(function (response) {
-    console.log("AvatarChanged",response);
+    console.log("AvatarChanged", response);
   });
 
-  avatarSelected="";
+  avatarSelected = "";
 
   document.getElementById("avatarImgInactive").src = (avatars[student.avatarId].avatarInactiveUrl).substring(1);
   document.getElementById("avatarImgBig").src = (avatars[student.avatarId].avatarBigUrl).substring(1);
@@ -455,4 +438,182 @@ function changeAvatar(){
 
 }
 
+function makeBubbleXXX(eduId, achived, chapterId, förderplanId) {
+  /*
+   if (chapterId == 0) {
+   document.getElementById("body1").style.backgroundColor = "#8da6d6";
+
+   var settingsCompetences = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://46.101.204.215:1337/api/V1/studentcompetence?checked=" + achived ? "true" : "false",
+   "method": "GET",
+   "headers": {
+   "authorization": token.token,
+   }
+   }
+   } else {
+
+   if (chapterId < 10) {
+   document.getElementById("flagImg").src = "images/chapter0" + chapterId + "/littleChapterFlag.png";
+   } else {
+   document.getElementById("flagImg").src = "images/chapter" + chapterId + "/littleChapterFlag.png";
+   }
+
+   var settingsCompetences = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://46.101.204.215:1337/api/V1/studentcompetence?checked=" + achived ? "true" : "false" + "&chapterId=" + chapterId,
+   "method": "GET",
+   "headers": {
+   "authorization": token.token,
+   }
+   }
+   }
+   if (förderplanId != -1) {
+
+   var settingsFoerderplanID = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://46.101.204.215:1337/api/V1/educationalPlan/:" + förderplanId,
+   "method": "GET",
+   "headers": {
+   "authorization": token.token,
+   }
+   }
+   }
+   else {
+
+   var settingsFoerderplan = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://46.101.204.215:1337/api/V1/educationalPlan",
+   "method": "GET",
+   "headers": {
+   "authorization": token.token,
+   }
+   }
+   }
+
+   var foerderplaene = [];
+   for (var i = 0; i < foerderplan.length; i++)
+   var settingsFoerderplane = {
+   "async": true,
+   "crossDomain": true,
+   "url": "http://46.101.204.215:1337/api/V1/educationalPlan/:" + i,
+   "method": "GET",
+   "headers": {
+   "authorization": token.token,
+   }
+   }
+   $.ajax(settingsFoerderplane).done(function (response) {
+   foerderplaene[i] = response;
+   });
+
+
+   var infoBubbleTXT = "";
+   var imgSRC = "";
+   var compIsFoerderplan = checkIfFoerderplan(competence, foerderPlaene);
+
+
+   var _id = foerderplan[0].competences[j].competenceId;
+
+   $("#todo_liste").append(
+   "<div class='bubble'>" +
+   "<div id='infoDiv' class='right'>" + "<div class='infoBubble'" + ">" +
+   "<div class='infoBubbleText'>" +
+   foerderplanTitel[planId].name + "<br>" +
+   foerderplan[0].competences[j].note +
+   "</div>" +
+   "</div>" + "</div>" +
+   "<div class='left'>" +
+   "<div class='right bubbleImgDiv'>" +
+   "<img class='bubbleImg'src=" + "images/educationalPlan-inactive.png" + ">" +
+   "</div>" +
+   "<div class='left'>" +
+   "<p>" + competences[_id].studentText + "</p>" + "" +
+   "<p class='number'>" + competences[_id].number + "</p>" +
+   "</div>" +
+   "</div>" +
+   "</div>");
+
+   $(".bubbleImgDiv").parent().parent().children('#infoDiv').hide();
+   $(".bubbleImg").attr("src", "images/educationalPlan-inactive.png");
+
+   $(".bubbleImgDiv").hover(
+   function () {
+   $(this).parent().parent().children('#infoDiv').show();
+   $(this).children('.bubbleImg').attr("src", "images/educationalPlan-active.png");
+
+   },
+   function () {
+   $(this).parent().parent().children('#infoDiv').hide();
+   $(this).children('.bubbleImg').attr("src", "images/educationalPlan-inactive.png");
+   }
+   );
+
+
+   }
+
+   */
+}
+
+function checkIfFoerderplan(competence, foerderPlaene) {
+
+
+  for (var i = 0; i < foerderPlaene.length; i++) {
+    for (var j = 0; j < foerderPlaene[i].competences.length; j++) {
+      if (foerderPlaene[i].competences[j].competenceId == competence.id) {
+        return true;
+      } else {
+        return false
+      }
+    }
+  }
+}
+
+function makeBubble(foerderplanTitel, foerderplanNote, imgActive, imgInActive, studentText, bubbleNumber,bubbleid) {
+
+  $("#todo_liste").append(
+    "<div class='bubble'>" +
+    "<div id='infoDiv' class='right'>" + "<div class='infoBubble'" + ">" +
+    "<div class='infoBubbleText'>" +
+    foerderplanTitel + "<br>" +
+    foerderplanNote +
+    "</div>" +
+    "</div>" + "</div>" +
+    "<div class='left'>" +
+    "<div class='right bubbleImgDiv"+ bubbleid +"'>" +
+    "<img class='bubbleImg'src=" + imgInActive + ">" +
+    "</div>" +
+    "<div class='left'>" +
+    "<p>" + studentText + "</p>" + "" +
+    "<p class='number'>" + bubbleNumber + "</p>" +
+    "</div>" +
+    "</div>" +
+    "</div>");
+
+  $(".bubbleImgDiv"+bubbleid).parent().parent().children('#infoDiv').hide();
+  $(".bubbleImgDiv"+bubbleid).children('.bubbleImg').attr("src", imgInActive);
+
+  console.log(bubbleNumber)
+  console.log("foerderNote= " + foerderplanNote);
+  console.log("foerderTitel= " + foerderplanTitel);
+
+  if (foerderplanNote != null && foerderplanNote != "null") {
+
+
+    $(".bubbleImgDiv"+bubbleid).hover(
+      function () {
+        $(this).parent().parent().children('#infoDiv').show();
+        $(this).children('.bubbleImg').attr("src", imgActive);
+
+      },
+      function () {
+        $(this).parent().parent().children('#infoDiv').hide();
+        $(this).children('.bubbleImg').attr("src", imgInActive);
+      }
+    );
+  }
+}
 
